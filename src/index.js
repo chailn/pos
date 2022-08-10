19887f09391874d4b0971f69365d9b722ce992c8
LAPTOP-9LIS77H4\chail <chailn8808@gmail.com>
2022-08-10 15:43:24 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./index.css";
 import App from "./App";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot.render(
+ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
       <App />
